55a7b01bb9cfa923a5cd95a10768939e47c1c141
kunjkumar Patel <kunjp19@gmail.com>
2024-08-10 06:02:09 -0400

    update server

--- tourServer/serverRun.js
+++
@@ -1,8 +0,0 @@
-const app = require('./tourServer'); // Import server
-const host = '192.168.86.249';
-const port = '3434';
-app.listen(port, host, function () {
-    console.log("Tour JSON session server listening on IPv4: " + host +
-        ":" + port);
-
-});
--- tourServer/tourDBRef.js
+++
@@ -1,4 +0,0 @@
-const DataStore = require('nedb-promises');
-// Create NEDB instance for tourDB links it to disk file
-let tourDB = DataStore.create(__dirname + '/tourDb');
-module.exports = tourDB;
